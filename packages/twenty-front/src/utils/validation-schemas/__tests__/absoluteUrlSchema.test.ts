3b6b7788ecb8828eb6720dcd30e503a9e99307ba
rostaklein <r.klein@make.com>
2024-07-10 10:24:01 +0200

    accept empty string in tests

--- packages/twenty-front/src/utils/validation-schemas/__tests__/absoluteUrlSchema.test.ts
+++ packages/twenty-front/src/utils/validation-schemas/__tests__/absoluteUrlSchema.test.ts
@@ -28,7 +28,10 @@ describe('absoluteUrlSchema', () => {
 
   it('fails for invalid urls', () => {
     expect(absoluteUrlSchema.safeParse('?o').success).toBe(false);
-    expect(absoluteUrlSchema.safeParse('').success).toBe(false);
     expect(absoluteUrlSchema.safeParse('\\').success).toBe(false);
+  });
+
+  it('should accept empty string', () => {
+    expect(absoluteUrlSchema.safeParse('').success).toBe(true);
   });
 });
